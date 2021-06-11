155b3923a4457a12df2135457f05658643d45fbf
enrique <77394830+Flower-fist@users.noreply.github.com>
2021-06-11 14:58:59 -0400

    Add files via upload

--- barracks.py
+++ barracks.py
@@ -1,5 +1,5 @@
-#Enrique made this.
-##Dredcode command class definition
+#~~Enrique made this.
+#Dredcode command class definition
 class command:
     def __init__(self, I,Am,A,Bm,B,O):
         self.instruct = I
@@ -8,18 +8,19 @@ class command:
         self.Bsmode   = Bm
         self.Bfield   = B 
         self.owner    = O
-#loads the constants from config
-with open(config.txt , 'r') as f:
-     *coresiz* = int(f.readline())
-     *timelim* = int(f.readline())
-     *sizelim* = int(f.readline())
-     *Dprefix* = f.readline()
-#this sets up the rules for Dredcode     
+#sets up the core constants
+coresiz = 8000
+timelim = 100000
+sizelim = 100
+Dprefix = 'Dredcode'
+
+#this sets up the rules for Dredcode
 legalCommands = ['dat','mov','add','sub','mod',
                  'jmp','jmz','jmn','seq','sne',
                  'slt','nop','spl']
 legalModes = ['~','#']
-##this converts a string to a command
+
+#this converts a string to a command
 def untypedSTC(temp, owner):
     output = command(temp[0],temp[1],temp[2],temp[3],temp[4],owner)
     return output
@@ -27,9 +28,9 @@ def stringToCommand(string, owner):
     temp = string.split()
     if temp[0] in legalCommands:
         if temp[1] and temp[3] in legalModes:
-            if isinstance(temp[2], int) and isinstance(temp[4], int)
-                temp[2] = int(temp[2]) % *core*
-                temp[4] = int(temp[4]) % *core*
+            if type(temp[2]) == int and type(temp[4]) == int
+                temp[2] = int(temp[2]) % coresiz
+                temp[4] = int(temp[4]) % coresiz
                 untypedSTC(temp, owner)
             else:
                 print('adress not number')
@@ -46,10 +47,11 @@ def stringToCommand(string, owner):
 print('Type the name of a warrior in the current directory,')
 print('it should be a DREDCODE program with the line "Dredcode" attached.')
 warrior = input()
+
 #actually loading the thing now that we know what it is
 with open(warrior, 'r') as f:
     #check if the warrior is viable(some how bugged)
-    if f.readline() == *Dprefix*:
+    if f.readline() == Dprefix:
         #the file loading
         f.seek(2)
         warrior = f.readline()
@@ -57,7 +59,7 @@ with open(warrior, 'r') as f:
             temp = f.readline()
             result = stringToCommand(temp, [0,0,0])
             append(result, warrior)
-            if i > *sizelim*:
+            if i > sizelim:
                 break
     else:
         print('It\'s not a redcode file.')
